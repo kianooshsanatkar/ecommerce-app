f27d36248b5cbdf3be333de774520a1199d73626
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-08 19:38:54 +0330

    initialize domain model

---
+++ domain/models/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["User", "Category", "Product", "Collection", "objects"]
+
+from .category import Category
+from .collection import Collection
+from .product import Product
+from .user import User
