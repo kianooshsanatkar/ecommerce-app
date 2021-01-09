50fe3cf762122958bcca19bd6af122d05ac5e860
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-09 20:10:34 +0330

    add __repr__ method to user class

--- domain/models/user.py
+++ domain/models/user.py
@@ -5,13 +5,17 @@ from ._db import Base
 from .entity import Entity
 
 
-class User(Entity, Base):
+class User(Base, Entity):
     __tablename__ = 'users'
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
     phone = Column(String, unique=True)
     email = Column(String, unique=True)
     addresses = relationship("Address", backref="users")
+
+    def __repr__(self):
+        return User.__name__ + "('" + self.first_name + "', '" + self.last_name + "', '" \
+               + self.phone + "', '" + self.email + "', " + str(self.uid) + ")"
 
 
 class Address(Entity, Base):
@@ -20,5 +24,5 @@ class Address(Entity, Base):
     city = Column(String, nullable=False)
     zip_code = Column(String, nullable=True)
     postal_address = Column(String, nullable=False)
-    user_id = Column(Integer, ForeignKey('user.uid'))
+    user_id = Column(Integer, ForeignKey('users.uid'))
     # user = relationship("user", back_populates="addresses")
