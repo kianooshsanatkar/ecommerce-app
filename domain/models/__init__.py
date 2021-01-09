d2137373f59e0ed298d1e166b54ab93cf80f0d01
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-09 20:08:04 +0330

    update domain model __init__.py

--- domain/models/__init__.py
+++ domain/models/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ["User", "Category", "Product", "Collection", "DBInitializer"]
+__all__ = ["User", "Category", "Product", "Collection", "DBInitializer", 'db_Base']
 
-from ._db import DBInitializer
+from ._db import DBInitializer, Base as db_Base
 from .category import Category
 from .collection import Collection
 from .product import Product
