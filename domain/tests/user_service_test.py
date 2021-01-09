098393e676fd9f1c313fcc1eb93ce515c69193dd
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-09 14:48:20 +0330

    Renaming: change convention class name for readability

--- domain/tests/user_service_test.py
+++ domain/tests/user_service_test.py
@@ -6,7 +6,7 @@ from domain.services import user_validation
 
 
 @dataclass(frozen=True)
-class TestUser:
+class MockUser:
     first_name: str
     last_name: str
     phone: str = None
@@ -18,13 +18,13 @@ class UserValidationTest(TestCase):
 
     def test_user_raise_exception_no_fist_name(self):
         with self.assertRaises(ValueException) as context:
-            user_validation(TestUser(first_name='', last_name='last name'))
+            user_validation(MockUser(first_name='', last_name='last name'))
         self.assertEqual('First Name is Required!', str(context.exception))
 
     def test_user_raise_exception_no_last_name(self):
         with self.assertRaises(ValueException) as context:
-            user_validation(TestUser(first_name='first name', last_name=''))
+            user_validation(MockUser(first_name='first name', last_name=''))
         self.assertEqual('Last Name is Required!', str(context.exception))
 
     def test_user_success(self):
-        self.assertTrue(user_validation(TestUser(first_name='first name', last_name='last name')))
+        self.assertTrue(user_validation(MockUser(first_name='first name', last_name='last name')))
