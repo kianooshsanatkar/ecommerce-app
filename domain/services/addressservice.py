6fa8e526853f53c1751384b6bdb6c920c790d1e3
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-09 14:12:57 +0330

    create Address Service : address_validation + related tests

---
+++ domain/services/addressservice.py
@@ -0,0 +1,15 @@
+from core.exceptions import ValueException
+from domain.models.user import Address
+
+
+def address_validation(address: Address):
+    if not address.provinces:
+        raise ValueException('province is required!')
+    if not address.city:
+        raise ValueException('city is required!')
+    if not address.zip_code or len(address.zip_code) != 10:
+        raise ValueException('zip code is required!')
+    if not address.postal_address or len(address.postal_address) >= 512:
+        raise ValueException('postal address is required!')
+    if not address.user_id:
+        raise ValueException('user_id is missing!')
---
+++ domain/tests/address_service_test.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from unittest import TestCase
+
+from core.exceptions import ValueException
+from domain.services import address_validation
+
+
+@dataclass(frozen=True)
+class MockAddress:
+    provinces: str
+    city: str
+    zip_code: str
+    postal_address: str
+    user_id: int
+
+
+class AddressValidationTest(TestCase):
+    def test_address_raise_value_exception_no_province(self):
+        self.assertRaises(ValueException,
+                          lambda: address_validation(
+                              MockAddress('', 'city', '0123456789', 'postal address', '1')))
+
+    def test_address_raise_value_exception_no_city(self):
+        self.assertRaises(ValueException,
+                          lambda: address_validation(
+                              MockAddress('province', '', '0123456789', 'postal address', 1)))
+
+    def test_address_raise_value_exception_no_zip_code(self):
+        self.assertRaises(ValueException,
+                          lambda: address_validation(
+                              MockAddress('province', 'city', '', 'postal address', 1)))
+
+    def test_address_raise_value_exception_zip_code_length(self):
+        self.assertRaises(ValueException,
+                          lambda: address_validation(
+                              MockAddress('province', 'city', '1' * 9, 'postal address', 1)))
+        self.assertRaises(ValueException,
+                          lambda: address_validation(
+                              MockAddress('province', 'city', '1' * 11, 'postal address', 1)))
+
+    def test_address_raise_value_exception_no_postal_address(self):
+        self.assertRaises(ValueException,
+                          lambda: address_validation(
+                              MockAddress('province', 'city', '0123456789', '', 1)))
+
+    def test_address_raise_value_exception_postal_address_length_maximum(self):
+        self.assertRaises(ValueException,
+                          lambda: address_validation(
+                              MockAddress('province', 'city', '0123456789', 'a' * 512, 1)))
+
+    def test_address_raise_value_exception_no_user_id(self):
+        self.assertRaises(ValueException,
+                          lambda: address_validation(
+                              MockAddress('province', 'city', '0123456789', 'postal address', None)))
