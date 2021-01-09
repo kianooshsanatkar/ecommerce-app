44906a874a16dc724eda56472e51ccab6fe1ecd0
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-09 17:56:58 +0330

    update userhandler.py: create get_user by id and email + related tests

--- handlers/tests/user_handler_test.py
+++ handlers/tests/user_handler_test.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from unittest import TestCase
 from unittest.mock import Mock
 
+from core.exceptions import TypeException
 from handlers.userhandler import UserHandler
 
 
@@ -19,6 +20,33 @@ class CreateUserHandlerTest(TestCase):
     def setUp(self) -> None:
         self.factory = Mock()
         UserHandler.Session = self.factory.session
+
+    def test_get_user_by_id(self):
+        # check if exception rises for the wrong data type
+        with self.assertRaises(TypeException) as _ex:
+            UserHandler.get_user_by_id('id')
+        self.assertEqual("uid parameter type must be int!", str(_ex.exception))
+        # check if session and query is called
+        UserHandler.get_user_by_id(1)
+        self.factory.session.assert_called()
+        self.factory.session().query.assert_called()
+
+    def test_get_user_by_email(self):
+        # check if exception rises for the wrong data type
+        with self.assertRaises(TypeException) as _ex:
+            UserHandler.get_user_by_email(1)
+        self.assertEqual("email parameter type must be string!", str(_ex.exception))
+
+        # check if email validation is called
+        UserHandler.email_validation = self.factory.email_validation
+        UserHandler.get_user_by_email('email')
+        self.factory.email_validation.assert_called()
+
+        # check if session and query is called
+        UserHandler.email_validation = self.factory.email_validation
+        UserHandler.get_user_by_email('email')
+        self.factory.session.assert_called()
+        self.factory.session().query.assert_called()
 
     def test_create_user_does_validation_called(self):
         UserHandler.user_validation = self.factory.validation
--- handlers/userhandler.py
+++ handlers/userhandler.py
@@ -1,12 +1,29 @@
+from core.exceptions import ValueException, TypeException
 from domain.models import DBInitializer
 from domain.models import User
-from domain.services import user_validation
+from domain.services import user_validation, email_validation
 
 
 class UserHandler:
 
     Session = DBInitializer.get_session
     user_validation = user_validation
+    email_validation = email_validation
+
+    @classmethod
+    def get_user_by_id(cls, uid: int) -> User:
+        if not isinstance(uid, int):
+            raise TypeException("uid parameter type must be int!")
+        session = cls.Session()
+        return session.query(User).get(uid)
+
+    @classmethod
+    def get_user_by_email(cls, email: str) -> User:
+        if not isinstance(email, str):
+            raise TypeException("email parameter type must be string!")
+        cls.email_validation(email)
+        session = cls.Session()
+        return session.query(User).filter_by(email=email).first()
 
     @classmethod
     def create_user(cls, user: User):
