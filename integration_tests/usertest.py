308a4ebb14e2f33bd37a85614a49b6c16c189b40
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-10 22:08:44 +0330

    update usertest.py

--- integration_tests/usertest.py
+++ integration_tests/usertest.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from unittest import TestCase
+
 from sqlalchemy.orm.session import close_all_sessions
 
 from domain.models import User
 from domain.models import db_Base as Base, DBInitializer
 from domain.services import user_validation, email_validation
+from domain.services.passwordservice import hashing_password
 from handlers import UserHandler
 
 
@@ -25,9 +27,10 @@ class UserDTO:
 class UserTest(TestCase):
 
     def setUpClass():
-        UserHandler.Session = DBInitializer.get_session
-        UserHandler.user_validation = user_validation
-        UserHandler.email_validation = email_validation
+        UserHandler._Session = DBInitializer.get_session
+        UserHandler._user_validation = user_validation
+        UserHandler._email_validation = email_validation
+        UserHandler._hashing = hashing_password
 
     def setUp(self) -> None:
         Base.metadata.create_all(bind=DBInitializer.get_engine())
@@ -37,7 +40,7 @@ class UserTest(TestCase):
         Base.metadata.drop_all(bind=DBInitializer.get_new_engine())
 
     def test_create_and_get_user(self):
-        user = User(first_name='first name', last_name='last name', phone='9121234567', email='email@domain.tld')
+        user = User(password='Pa$$w0rd', first_name='first name', last_name='last name', phone='9121234567', email='email@domain.tld')
         # print(user)
         uid = UserHandler.create_user(user)
         u = UserHandler.get_user_by_id(uid)
@@ -48,7 +51,7 @@ class UserTest(TestCase):
         self.assertEqual(user.email, u.email)
 
     def test_create_user_and_get_by_email(self):
-        user = User(first_name='first name', last_name='last name', phone='9121234567', email='email@domain.tld')
+        user = User(password='Pa$$w0rd', first_name='first name', last_name='last name', phone='9121234567', email='email@domain.tld')
         # print(user)
         UserHandler.create_user(user)
         u = UserHandler.get_user_by_email('email@domain.tld')
@@ -57,3 +60,8 @@ class UserTest(TestCase):
         self.assertEqual(user.last_name, u.last_name)
         self.assertEqual(user.phone, u.phone)
         self.assertEqual(user.email, u.email)
+
+    # todo: login scenario:
+    #  - password verification check
+
+    # todo: password should not return from get_user either from email or id
