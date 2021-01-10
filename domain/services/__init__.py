c6df068c9d2d7849450e417d76b044a7133ca564
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-11 01:57:55 +0330

    rename contactservice.py

--- domain/services/__init__.py
+++ domain/services/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['user_validation', 'phone_validation', 'email_validation', 'address_validation', 'passwordservice']
 
 from domain.services.addressservice import address_validation
-from domain.services.contactservices import phone_validation, email_validation
+from domain.services.contactservice import phone_validation, email_validation
 from domain.services.userservices import user_validation
