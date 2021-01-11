68f2d5a0e29d149c35eb340c3c33a9593a88a974
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-11 03:59:31 +0330

    write integration test for "password should not return" backlog: https://trello.com/c/w93NybqE

--- integration_tests/usertest.py
+++ integration_tests/usertest.py
@@ -3,9 +3,10 @@ from unittest import TestCase
 
 from sqlalchemy.orm.session import close_all_sessions
 
+from core.exceptions import AuthenticationException
 from domain.models import User
 from domain.models import db_Base as Base, DBInitializer
-from domain.services import user_validation, email_validation
+from domain.services import user_validation, email_validation, passwordservice
 from domain.services.passwordservice import hashing_password
 from handlers import UserHandler
 
@@ -26,11 +27,13 @@ class UserDTO:
 
 class UserTest(TestCase):
 
+    @staticmethod
     def setUpClass():
         UserHandler._Session = DBInitializer.get_session
         UserHandler._user_validation = user_validation
         UserHandler._email_validation = email_validation
         UserHandler._hashing = hashing_password
+        UserHandler._password_service = passwordservice
 
     def setUp(self) -> None:
         Base.metadata.create_all(bind=DBInitializer.get_engine())
@@ -40,28 +43,28 @@ class UserTest(TestCase):
         Base.metadata.drop_all(bind=DBInitializer.get_new_engine())
 
     def test_create_and_get_user(self):
-        user = User(password='Pa$$w0rd', first_name='first name', last_name='last name', phone='9121234567', email='email@domain.tld')
-        # print(user)
+        user = User(password='Pa$$w0rd', first_name='first name', last_name='last name', phone='9121234567',
+                    email='email@domain.tld')
         uid = UserHandler.create_user(user)
         u = UserHandler.get_user_by_id(uid)
-        # print(u)
+        # assert user properties with origin properties
         self.assertEqual(user.first_name, u.first_name)
         self.assertEqual(user.last_name, u.last_name)
         self.assertEqual(user.phone, u.phone)
         self.assertEqual(user.email, u.email)
+        # check if password is null
+        self.assertIsNone(u.password)
 
     def test_create_user_and_get_by_email(self):
-        user = User(password='Pa$$w0rd', first_name='first name', last_name='last name', phone='9121234567', email='email@domain.tld')
-        # print(user)
+        user = User(password='Pa$$w0rd', first_name='first name', last_name='last name', phone='9121234567',
+                    email='email@domain.tld')
         UserHandler.create_user(user)
         u = UserHandler.get_user_by_email('email@domain.tld')
-        # print(u)
+        # assert user properties with origin properties
         self.assertEqual(user.first_name, u.first_name)
         self.assertEqual(user.last_name, u.last_name)
         self.assertEqual(user.phone, u.phone)
         self.assertEqual(user.email, u.email)
+        # check if password is null
+        self.assertIsNone(u.password)
 
-    # todo: login scenario:
-    #  - password verification check
-
-    # todo: password should not return from get_user either from email or id
