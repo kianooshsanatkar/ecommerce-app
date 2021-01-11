0205db09431f902309558d7dca2171c1a40047d0
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-12 01:00:45 +0330

    update Model __init__.py https://trello.com/c/tXSUdc1C

--- domain/models/__init__.py
+++ domain/models/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ["User", "Category", "Product", "Collection", "DBInitializer", 'db_Base']
+__all__ = ["User", "Category", "Product", "Collection", "DBInitializer", 'db_Base', "Token"]
 
 from ._db import DBInitializer, Base as db_Base
 from .category import Category
 from .collection import Collection
 from .product import Product
+from .token import Token
 from .user import User
