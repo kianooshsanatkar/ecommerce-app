9e673a200f44be0daa64ca823e96e84545805f1a
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-13 22:06:45 +0330

    last_used column is null at first https://trello.com/c/tXSUdc1C

--- domain/models/token.py
+++ domain/models/token.py
@@ -18,5 +18,5 @@ class Token(Entity, Base):
     requested_time = Column(DateTime, nullable=False)
     time_limit = Column(DateTime, nullable=False, default=datetime.utcnow() + TIME_SPAN)
     failed_attempts = Column(Integer, nullable=False, default=0)
-    last_used_time = Column(DateTime, nullable=False)
+    last_used_time = Column(DateTime, nullable=True)
     deactivate = Column(Boolean, nullable=False, default=False)
