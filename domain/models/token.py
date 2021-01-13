732b5d8e4dc792fdd63c675b5144e8d1a49ff5b8
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-13 19:27:04 +0330

    update token.py https://trello.com/c/tXSUdc1C

--- domain/models/token.py
+++ domain/models/token.py
@@ -1,15 +1,22 @@
-from sqlalchemy import Column, String, DateTime, Integer, ForeignKey
+from datetime import timedelta, datetime
+
+from sqlalchemy import Column, String, DateTime, Integer, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
 from domain.models._db import Base
 from domain.models.entity import Entity
 
+TIME_SPAN = timedelta(hours=1)
+
 
 class Token(Entity, Base):
     __tablename__ = 'tokens'
-    token_hex = Column(String, nullable=False)
-    token_url = Column(String, nullable=False)
+    hex_token = Column(String, nullable=False)
+    url_token = Column(String, nullable=False)
     user_id = Column(Integer, ForeignKey('users.uid'), nullable=False)
     user = relationship('User', backref='tokens')
     requested_time = Column(DateTime, nullable=False)
-    used_time = Column(DateTime, nullable=False)
+    time_limit = Column(DateTime, nullable=False, default=datetime.utcnow() + TIME_SPAN)
+    failed_attempts = Column(Integer, nullable=False, default=0)
+    last_used_time = Column(DateTime, nullable=False)
+    deactivate = Column(Boolean, nullable=False, default=False)
