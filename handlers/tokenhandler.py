f7329cf2db30c11e89bee1620e2cd7c790fc3622
Kyanoush <kianoosh.sanatkar@yahoo.com>
2021-01-15 03:50:12 +0330

    update Token Handler https://trello.com/c/KpScuRxV

--- handlers/tokenhandler.py
+++ handlers/tokenhandler.py
@@ -1,18 +1,12 @@
 import secrets
 from datetime import datetime
-from enum import Enum
 
 from sqlalchemy import desc
 
 from core.exceptions import SecurityException, TimeoutException, AuthenticationException, InnerException
-from domain.models import DBInitializer, User
+from domain.models import DBInitializer
 from domain.models import Token
-
-
-class TokenVia(Enum):
-    phone_number = 1
-    email_address = 2
-    both = 3
+from domain.models.token import ExchangeMethods
 
 
 def get_user_builder(user_id):
@@ -25,28 +19,39 @@ class TokenHandler:
     _get_user = get_user_builder
 
     @classmethod
-    def generate_token(cls, user_id: int, via: TokenVia) -> bool:
+    def generate_token(cls, user_id: int, via: ExchangeMethods) -> bool:
         u = cls._get_user(user_id)
         if not u:
-            raise SecurityException("User doesn't exist!")
+            raise SecurityException(f"User doesn't exist! user_id: {user_id}")
+        if via == ExchangeMethods.PHONE and not u.phone:
+            raise InnerException('User phone is not registered')
+        if via == ExchangeMethods.EMAIL and not u.email:
+            raise InnerException('User email is not registered')
         session = cls._Session()
 
         last_token = session.query(Token).filter_by(user_id=user_id).order_by(desc(Token.requested_time)).first()
 
-        if last_token and not last_token.deactivate and datetime.utcnow() < last_token.time_limit:
+        if last_token and not last_token.deactivate and last_token.exchange_method == via.value \
+                and datetime.utcnow() < last_token.time_limit:
             raise InnerException('A valid token already issued!')
         token = Token(user_id=u.uid, requested_time=datetime.utcnow())
         token.hex_token = secrets.token_hex(2)
         token.url_token = secrets.token_urlsafe()
+        token.exchange_method = via.value
         session.add(token)
         session.commit()
         # Todo: ******* send the token to the service provider (MailService|SMSService) ********
         return True
 
     @classmethod
-    def hexadecimal_token_validation(cls, user_id, auth_token: str) -> bool:
+    def hexadecimal_token_validation(cls, user_id, auth_token: str, exchange_method: ExchangeMethods = None) -> bool:
         session = cls._Session()
-        token = session.query(Token).filter_by(user_id=user_id).order_by(desc(Token.requested_time)).first()
+        token = None
+        if exchange_method:
+            token = session.query(Token).filter_by(user_id=user_id, exchange_method=exchange_method.value).order_by(
+                desc(Token.requested_time)).first()
+        else:
+            token = session.query(Token).filter_by(user_id=user_id).order_by(desc(Token.requested_time)).first()
         if not token:
             raise SecurityException('User has no token!')
         if token.deactivate:
@@ -56,7 +61,7 @@ class TokenHandler:
         if secrets.compare_digest(token.hex_token, auth_token):
             token.last_used_time = datetime.utcnow()
             session.commit()
-            return True
+            return token.exchange_method
         token.failed_attempts += 1
         if token.failed_attempts > 3:
             token.deactivate = True
@@ -66,7 +71,7 @@ class TokenHandler:
         raise AuthenticationException('Token is not valid!')
 
     @classmethod
-    def url_token_validation(cls, url_token: str) -> User:
+    def url_token_validation(cls, url_token: str) -> tuple:
         session = cls._Session()
         tk = session.query(Token).filter_by(url_token=url_token).first()
         if not tk:
@@ -76,4 +81,4 @@ class TokenHandler:
         if datetime.utcnow() > tk.time_limit:
             raise TimeoutException('Token is Expired!')
         tk.last_used_time = datetime.utcnow()
-        return tk.user
+        return tk.user.uid, tk.exchange_method
